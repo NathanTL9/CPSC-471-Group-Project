adf810d37e06182e80eaa312f36a526348f88657
0wenH <owen.hunter@ucalgary.ca>
2021-04-02 13:04:33 -0600

    Database

--- clubs/admin.py
+++ clubs/admin.py
@@ -3,8 +3,22 @@ from django.contrib import admin
 from .models import Club
 from .models import Event
 from .models import Announcment
+from .models import ClubRepresentative
+from .models import ClubMember
+from .models import Reserves
+from .models import Room
+from .models import Student
+from .models import University
+
 
 admin.site.register(Club)
 admin.site.register(Event)
 admin.site.register(Announcment)
+admin.site.register(ClubMember)
+admin.site.register(ClubRepresentative)
+admin.site.register(Room)
+admin.site.register(Reserves)
+admin.site.register(Student)
+admin.site.register(University)
+
 
--- clubs/models.py
+++ clubs/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from datetime import datetime 
+from django.contrib.auth.models import User
 
 class Club(models.Model):
-    club_name = models.CharField(max_length=200)
+    club_name = models.CharField(max_length=200, primary_key = True)
     club_type = models.CharField(max_length=200, default = "none")
     club_budget = models.IntegerField(default= 0)
     contactEmail = models.EmailField(default = "nullnull@null.ca")
@@ -11,8 +12,32 @@ class Club(models.Model):
     def __str__(self):
         return self.club_name
 
+
+class University(models.Model):
+    schoolID = models.CharField(max_length=200, primary_key = True)
+    name = models.CharField(max_length=200, default = "none")
+    location = models.CharField(max_length=200, default = "none")
+
+    def __str__(self):
+        return self.schoolID
+
+
+class Room(models.Model):
+    RoomNo = models.IntegerField(default = 0, primary_key = True)
+    schoolID = models.ForeignKey(University, to_field='schoolID', on_delete=models.CASCADE)
+    buildingName = models.CharField(max_length=200, default = "none")
+    Capacity = models.IntegerField(default= 0)
+    location = models.CharField(max_length=200, default = "none")
+
+
+
+    def __str__(self):
+        return self.RoomNo
+
+
 class Event(models.Model):
-    clubname = models.ForeignKey(Club, on_delete=models.CASCADE)
+    clubname = models.ForeignKey(Club, to_field='club_name', on_delete=models.CASCADE)
+    RoomNo = models.ForeignKey(Room, to_field='RoomNo', on_delete=models.CASCADE)
     date = models.DateTimeField('Event date', default=datetime.now)
     event_type = models.CharField(max_length=200, default = "none")
     description = models.CharField(max_length=200, default = "none")
@@ -23,9 +48,64 @@ class Event(models.Model):
 
 
 class Announcment(models.Model):
-    clubname = models.ForeignKey(Club, on_delete=models.CASCADE)
+    clubname = models.ForeignKey(Club, to_field='club_name', on_delete=models.CASCADE)
     date = models.DateTimeField('Event date', default=datetime.now)
     message = models.CharField(max_length=200, default = "none")
 
     def __str__(self):
         return self.message
+
+
+class Student(models.Model):
+    studentID = models.CharField(max_length = 9, default = 0, primary_key = True)
+    schoolID = models.ForeignKey(University, to_field='schoolID', on_delete=models.CASCADE)
+    UserID =  models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, default = "none")
+    Year_of_study = models.IntegerField(default= 0)
+    major = models.CharField(max_length=200, default = "none")
+    phoneNumber = models.CharField(max_length=200, default = "none")
+
+
+
+    def __str__(self):
+        return self.studentID
+
+
+class ClubMember(models.Model):
+    clubname = models.ForeignKey(Club, to_field='club_name', on_delete=models.CASCADE)
+    studnetID = models.ForeignKey(Student, to_field='studentID', on_delete=models.CASCADE)
+    MemberID = models.CharField(max_length=200, default = "none")
+    
+
+    def __str__(self):
+        return self.studentID
+
+
+class ClubRepresentative(models.Model):
+    clubname = models.ForeignKey(Club, to_field='club_name', on_delete=models.CASCADE)
+    studnetID = models.ForeignKey(Student, to_field='studentID', on_delete=models.CASCADE)
+    Tresurer = models.BooleanField()
+    VicePresident = models.BooleanField()
+    President = models.BooleanField()
+
+
+    
+
+    def __str__(self):
+        return self.studentID
+
+
+
+class Reserves(models.Model):
+    
+    clubname = models.ForeignKey(Club, to_field='club_name', on_delete=models.CASCADE)
+    RoomNo = models.ForeignKey(Room, to_field='RoomNo', on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.RoomNo
+
+
+
+
+
